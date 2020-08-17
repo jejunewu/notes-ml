90339246aaada964981aff3c9d46296fb696d477
jejune5 <752023928@qq.com>
2020-08-17 14:10:06 +0800

    Update 损失函数及梯度.py

--- LongNote/CH7/损失函数及梯度.py
+++ LongNote/CH7/损失函数及梯度.py
@@ -2,13 +2,15 @@ import tensorflow as tf
 
 x = tf.random.normal([2, 4])
 w = tf.random.normal([4, 3])
-
 b = tf.constant([10., 20., 30.])
+
+y = tf.constant([2, 0])
 
 with tf.GradientTape() as tape:
     tape.watch([w, b])
     prob = tf.nn.softmax(x@w+b, axis=1)
-    loss = tf.reduce_mean()
+    loss = tf.reduce_mean(tf.losses.MSE(tf.one_hot(y, depth=3), prob))
 
-y = x@w + b
-print(y)
+grads = tape.gradient(loss, [w, b])
+
+print(grads)
