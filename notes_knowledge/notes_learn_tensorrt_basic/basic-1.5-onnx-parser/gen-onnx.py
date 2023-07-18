de5440093150d067a3817bed3404b1fd69a834f6
jejune5 <752023928@qq.com>
2023-07-18 16:56:16 +0800

    update

--- notes_knowledge/notes_learn_tensorrt_basic/basic-1.5-onnx-parser/gen-onnx.py
+++ notes_knowledge/notes_learn_tensorrt_basic/basic-1.5-onnx-parser/gen-onnx.py
@@ -4,6 +4,7 @@ import torch.nn.functional as F
 import torch.onnx
 import os
 
+
 class Model(torch.nn.Module):
     def __init__(self):
         super().__init__()
@@ -12,11 +13,12 @@ class Model(torch.nn.Module):
         self.relu = nn.ReLU()
         self.conv.weight.data.fill_(1)
         self.conv.bias.data.fill_(0)
-    
+
     def forward(self, x):
         x = self.conv(x)
         x = self.relu(x)
         return x
+
 
 # 这个包对应opset11的导出代码，如果想修改导出的细节，可以在这里修改代码
 # import torch.onnx.symbolic_opset11
@@ -25,23 +27,23 @@ print("对应opset文件夹代码在这里：", os.path.dirname(torch.onnx.__fil
 model = Model()
 dummy = torch.zeros(1, 1, 3, 3)
 torch.onnx.export(
-    model, 
+    model,
 
     # 这里的args，是指输入给model的参数，需要传递tuple，因此用括号
-    (dummy,), 
+    (dummy,),
 
     # 储存的文件路径
-    "workspace/demo.onnx", 
+    "workspace/demo.onnx",
 
     # 打印详细信息
-    verbose=True, 
+    verbose=True,
 
     # 为输入和输出节点指定名称，方便后面查看或者操作
-    input_names=["image"], 
-    output_names=["output"], 
+    input_names=["image"],
+    output_names=["output"],
 
     # 这里的opset，指，各类算子以何种方式导出，对应于symbolic_opset11
-    opset_version=11, 
+    opset_version=11,
 
     # 表示他有batch、height、width3个维度是动态的，在onnx中给其赋值为-1
     dynamic_axes={
