e1001a9aec60807a0373ad46bf8d284a5c600888
jejune5 <752023928@qq.com>
2023-07-24 09:52:33 +0800

    update

--- notes_knowledge/notes_learn_tensorrt_basic/basic-1.5-onnx-parser/main.cpp
+++ notes_knowledge/notes_learn_tensorrt_basic/basic-1.5-onnx-parser/main.cpp
@@ -22,21 +22,28 @@
 
 using namespace std;
 
-inline const char* severity_string(nvinfer1::ILogger::Severity t){
-    switch(t){
-        case nvinfer1::ILogger::Severity::kINTERNAL_ERROR: return "internal_error";
-        case nvinfer1::ILogger::Severity::kERROR:   return "error";
-        case nvinfer1::ILogger::Severity::kWARNING: return "warning";
-        case nvinfer1::ILogger::Severity::kINFO:    return "info";
-        case nvinfer1::ILogger::Severity::kVERBOSE: return "verbose";
-        default: return "unknow";
+inline const char *severity_string(nvinfer1::ILogger::Severity t) {
+    switch (t) {
+        case nvinfer1::ILogger::Severity::kINTERNAL_ERROR:
+            return "internal_error";
+        case nvinfer1::ILogger::Severity::kERROR:
+            return "error";
+        case nvinfer1::ILogger::Severity::kWARNING:
+            return "warning";
+        case nvinfer1::ILogger::Severity::kINFO:
+            return "info";
+        case nvinfer1::ILogger::Severity::kVERBOSE:
+            return "verbose";
+        default:
+            return "unknow";
     }
 }
 
-class TRTLogger : public nvinfer1::ILogger{
+// 定义logger
+class TRTLogger : public nvinfer1::ILogger {
 public:
-    virtual void log(Severity severity, nvinfer1::AsciiChar const* msg) noexcept override{
-        if(severity <= Severity::kINFO){
+    virtual void log(Severity severity, nvinfer1::AsciiChar const *msg) noexcept override {
+        if (severity <= Severity::kINFO) {
             // 打印带颜色的字符，格式如下：
             // printf("\033[47;33m打印的文本\033[0m");
             // 其中 \033[ 是起始标记
@@ -47,13 +54,11 @@ public:
             //      \033[0m 是终止标记
             // 其中背景颜色或者文字颜色可不写
             // 部分颜色代码 https://blog.csdn.net/ericbar/article/details/79652086
-            if(severity == Severity::kWARNING){
+            if (severity == Severity::kWARNING) {
                 printf("\033[33m%s: %s\033[0m\n", severity_string(severity), msg);
-            }
-            else if(severity <= Severity::kERROR){
+            } else if (severity <= Severity::kERROR) {
                 printf("\033[31m%s: %s\033[0m\n", severity_string(severity), msg);
-            }
-            else{
+            } else {
                 printf("%s: %s\n", severity_string(severity), msg);
             }
         }
@@ -61,25 +66,30 @@ public:
 } logger;
 
 // 上一节的代码
-bool build_model(){
+bool build_model() {
     TRTLogger logger;
 
     // ----------------------------- 1. 定义 builder, config 和network -----------------------------
-    nvinfer1::IBuilder* builder = nvinfer1::createInferBuilder(logger);
-    nvinfer1::IBuilderConfig* config = builder->createBuilderConfig();
-    nvinfer1::INetworkDefinition* network = builder->createNetworkV2(1);
+    nvinfer1::IBuilder *builder = nvinfer1::createInferBuilder(logger);
+    nvinfer1::IBuilderConfig *config = builder->createBuilderConfig();
+    nvinfer1::INetworkDefinition *network = builder->createNetworkV2(1);
 
 
     // ----------------------------- 2. 输入，模型结构和输出的基本信息 -----------------------------
+    /**
+     * 解析onnx模型，
+     *      - NvOnnxParser.h --> createParser
+     *
+     */
     // 通过onnxparser解析的结果会填充到network中，类似addConv的方式添加进去
-    nvonnxparser::IParser* parser = nvonnxparser::createParser(*network, logger);
-    if(!parser->parseFromFile("demo.onnx", 1)){
+    nvonnxparser::IParser *parser = nvonnxparser::createParser(*network, logger);
+    if (!parser->parseFromFile("demo.onnx", 1)) {
         printf("Failed to parser demo.onnx\n");
 
         // 注意这里的几个指针还没有释放，是有内存泄漏的，后面考虑更优雅的解决
         return false;
     }
-    
+
     int maxBatchSize = 10;
     printf("Workspace Size = %.2f MB\n", (1 << 28) / 1024.0f / 1024.0f);
     config->setMaxWorkspaceSize(1 << 28);
@@ -89,24 +99,27 @@ bool build_model(){
     auto profile = builder->createOptimizationProfile();
     auto input_tensor = network->getInput(0);
     int input_channel = input_tensor->getDimensions().d[1];
-    
+
     // 配置输入的最小、最优、最大的范围
-    profile->setDimensions(input_tensor->getName(), nvinfer1::OptProfileSelector::kMIN, nvinfer1::Dims4(1, input_channel, 3, 3));
-    profile->setDimensions(input_tensor->getName(), nvinfer1::OptProfileSelector::kOPT, nvinfer1::Dims4(1, input_channel, 3, 3));
-    profile->setDimensions(input_tensor->getName(), nvinfer1::OptProfileSelector::kMAX, nvinfer1::Dims4(maxBatchSize, input_channel, 5, 5));
+    profile->setDimensions(input_tensor->getName(), nvinfer1::OptProfileSelector::kMIN,
+                           nvinfer1::Dims4(1, input_channel, 3, 3));
+    profile->setDimensions(input_tensor->getName(), nvinfer1::OptProfileSelector::kOPT,
+                           nvinfer1::Dims4(1, input_channel, 3, 3));
+    profile->setDimensions(input_tensor->getName(), nvinfer1::OptProfileSelector::kMAX,
+                           nvinfer1::Dims4(maxBatchSize, input_channel, 5, 5));
     // 添加到配置
     config->addOptimizationProfile(profile);
 
-    nvinfer1::ICudaEngine* engine = builder->buildEngineWithConfig(*network, *config);
-    if(engine == nullptr){
+    nvinfer1::ICudaEngine *engine = builder->buildEngineWithConfig(*network, *config);
+    if (engine == nullptr) {
         printf("Build engine failed.\n");
         return false;
     }
 
     // -------------------------- 3. 序列化 ----------------------------------
     // 将模型序列化，并储存为文件
-    nvinfer1::IHostMemory* model_data = engine->serialize();
-    FILE* f = fopen("engine.trtmodel", "wb");
+    nvinfer1::IHostMemory *model_data = engine->serialize();
+    FILE *f = fopen("engine.trtmodel", "wb");
     fwrite(model_data->data(), 1, model_data->size(), f);
     fclose(f);
 
@@ -121,7 +134,7 @@ bool build_model(){
     return true;
 }
 
-int main(){
+int main() {
     build_model();
     return 0;
 }
